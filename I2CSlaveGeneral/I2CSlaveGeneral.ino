e9d3253b06e61c77d017d6e6f0dbc69b1e9c45ba
jakezimmer <jzcomputershare@gmail.com>
2015-08-17 11:18:56 -0700

    Added tons of I2C base stuff

--- I2CSlaveGeneral/I2CSlaveGeneral.ino
+++ I2CSlaveGeneral/I2CSlaveGeneral.ino
@@ -12,18 +12,22 @@ size_t addr;
 
 
 void setup() {
-  delay(2000);
+  delay(500);
   
   Timer1.initialize(6);
   phil();
   Timer1.start();
   Timer1.pwm(4, 700);
+  
   // Setup for Slave mode, address 0x44, pins 18/19, external pullups, 400kHz
   // Each slave must have its own address, designate in hex.
   Wire.begin(I2C_SLAVE, 0x01, I2C_PINS_18_19, I2C_PULLUP_EXT, I2C_RATE_400);
   memset(sensorsTotal, 0, sizeof(sensorsTotal));
   cmd = 0;
   addr = 0;
+
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
 }
 
 void loop() {
@@ -34,4 +38,41 @@ void loop() {
   }
   
   
+}
+
+void receiveEvent(size_t len)
+{
+    if(Wire.available())
+    {
+        // grab command
+        cmd = Wire.readByte();
+        switch(cmd)
+        {
+        case WRITE:
+            addr = Wire.readByte();                // grab addr
+            while(Wire.available())
+                if(addr < MEM_LEN)                 // drop data beyond mem boundary
+                    mem[addr++] = Wire.readByte(); // copy data to mem
+                else
+                    Wire.readByte();               // drop data if mem full
+            break;
+
+        case READ:
+            addr = Wire.readByte();                // grab addr
+            break;
+        }
+    }
+}
+
+//
+// handle Tx Event (outgoing I2C data)
+//
+void requestEvent(void)
+{
+    switch(cmd)
+    {
+    case READ:
+        Wire.write(&mem[addr], MEM_LEN-addr); // fill Tx buffer (from addr location to end of mem)
+        break;
+    }
 }
